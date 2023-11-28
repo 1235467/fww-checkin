ec2211e021c74e336aca7eeca4e601573055de08
hakutaku <qq20099@outlook.com>
2023-11-28 10:00:36 +0800

    forgot to add node-env folder

--- default.nix
+++ default.nix
@@ -8,13 +8,13 @@
 }:
 
 let
-  nodeEnv = import ./node-env.nix {
+  nodeEnv = import ./node-env/node-env.nix {
     inherit (pkgs) stdenv lib python2 runCommand writeTextFile writeShellScript;
     inherit pkgs nodejs;
     libtool = if pkgs.stdenv.isDarwin then pkgs.darwin.cctools else null;
   };
 in
-import ./node-packages.nix {
+import ./node-env/node-packages.nix {
   inherit (pkgs) fetchurl nix-gitignore stdenv lib fetchgit;
   inherit nodeEnv;
 }
